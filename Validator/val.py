8e6f0a1241824a600d2c78c6922a2d9a598dccc0
JJ-vle <jeanjacquesviale.txt@gmail.com>
2025-10-26 17:18:02 +0100

    validator duplicated key detection

--- Validator/val.py
+++ Validator/val.py
@@ -2,6 +2,15 @@ import json
 import argparse
 from jsonschema import validate, ValidationError
 
+# verif qu'il y a pas de doublon de clé dans JSON
+def no_duplicate_object_pairs_hook(pairs):
+    obj = {}
+    for key, value in pairs:
+        if key in obj:
+            raise ValueError(f"Clé JSON dupliquée détectée : '{key}'")
+        obj[key] = value
+    return obj
+
 # parser arguments
 parser = argparse.ArgumentParser(description="Valider un fichier JSON selon le schema attendu.")
 parser.add_argument(
@@ -16,12 +25,15 @@ args = parser.parse_args()
 # lecture JSON
 try:
     with open(args.json_file, "r", encoding="utf-8") as f:
-        data = json.load(f)
+        data = json.load(f, object_pairs_hook=no_duplicate_object_pairs_hook)
 except FileNotFoundError:
     print(f"Fichier introuvable : {args.json_file}")
     exit(1)
 except json.JSONDecodeError as e:
     print(f"Erreur de syntaxe JSON dans le fichier : {e}")
+    exit(1)
+except ValueError as e:  # doublons détectés
+    print(f"Erreur : {e}")
     exit(1)
 
 # schema JSON
